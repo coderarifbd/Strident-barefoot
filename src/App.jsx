b23c6381c7747c402b73349e88c495689ca32037
coderarifbd <coder.arifbd@gmail.com>
2026-07-30 00:41:13 +0600

    Fix GitHub Pages white screen by adding base path and HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect } from 'react';
 import Header from './components/Header';
@@ -101,8 +101,8 @@ function AppInner() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppInner />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
